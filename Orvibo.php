5efa20204a8c19b5f69ecfdaf55722e573cd194d
Matthew Emerick-Law <matt@emericklaw.co.uk>
2016-08-31 15:01:57 +0100

    Added ascii2hex helper function

--- Orvibo.php
+++ Orvibo.php
@@ -89,4 +89,14 @@ class Orvibo
       }
     }
   }
+
+  public function ascii2hex($ascii) {
+    $hex = '';
+    for ($i = 0; $i < strlen($ascii); $i++) {
+      $byte = strtoupper(dechex(ord($ascii{$i})));
+      $byte = str_repeat('0', 2 - strlen($byte)).$byte;
+      $hex.=$byte." ";
+    }
+    return $hex;
+  }
 }
